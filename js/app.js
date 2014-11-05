4aa537b33a0a527b4e0f9471a6236dc8efdbeb66
kourtniejsanders <kourtniejoi@aol.com>
2014-11-05 11:15:45 -0800

    Third commit of files
    Additional changes made

--- js/app.js
+++ js/app.js
@@ -4,17 +4,7 @@ $(document).ready(function(){
 		alert('Congratulations! You have just earned 20% off. Just mention the coupon code: NETWINNING20, to one of our sales associates and a 20% discount will be applied to a future purchase.');
 	}); //end couponcode click
 	
-	$('#downloadables-btn').hover(function(){
-		console.log('The downloadables button was hovered over!');
-		$('#downloadables-btn').css({ 
-		backgroundColor:'black'
-		});// end css
-	},function(){
-		console.log('The downloadables button was left behind!');
-		$('#downloadables-btn').css({ 
-		backgroundColor:'green'
-		});// end css
-	}); //end hover
+
 
 	$('#qrcode-btn-off').click(function(){
     $('#qrcode-img').hide(500);
@@ -34,7 +24,7 @@ $(document).ready(function(){
     			opacity: '1'
     		}); //end hero img css
     		$('#hero-btn').css({
-    			backgroundColor: 'green'
+    			backgroundColor: '#edd219'
     		}); //end hero button css
     	}); //end hero animate
     }); //end hero click
@@ -86,5 +76,22 @@ $('button').click(function(submit){
         }
 
 });//end submit click
+
+$('#kourtport-img').hover(function(){
+    $('#kourtport-img').attr({
+        'src': 'img/happyguyhover.png',
+        'class' : 'img-responsive img-circle'
+    });//end attr
+}, function(){
+     $('#kourtport-img').attr({
+        'src': 'img/kourtniesanders.png',
+        'class' : 'img-responsive'
+    
+    });//end attr
+
+});//end of kourtport hover
+
+
+
 
 }); //end ready
